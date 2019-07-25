7c841d892dff0f43fd68438bc5f24c84c16f5e2d
lorranyhengles <lorranyhengles@gmail.com>
2019-07-25 17:04:00 -0300

    criar forms,objects

--- website/views.py
+++ website/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
+from website.models import Pessoa
 
 # Create your views here.
 def index(request):
-    args={
-
-    }
-    return render(request,"index.html", args)
+    contexto={}
+    if request.method =='POST':
+        pessoa=Pessoa()
+        pessoa.nome= request.POST.get('nome')
+        pessoa.sobrenome= request.POST.get('sobrenome')
+        pessoa.email= request.POST.get('email')
+        pessoa.genero= request.POST.get('genero')
+        pessoa.biografia= request.POST.get('biografia')
+        pessoa.save()
+        (pessoa)
+        print('opaaa salvouu')
+    contexto={'msg':'Parabensss Gatinhaaa'}
+    return render(request,"index.html", contexto)
 
 def sobre(request):
+    pessoa= Pessoa.objects.all()
     args={
-
+        'pessoas':pessoa
     }
     return render(request,"sobre.html", args)
+
